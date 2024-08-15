ce67ba2fdce027acd2db53cc07b413b3db542cd3
Ananias Raphael <ananias.nobrega@gmail.com>
2024-08-15 00:32:02 -0300

    resolve: #12 #11 #10 #9 issues implementations

--- supplier/api/v1/supplier/urls.py
+++ supplier/api/v1/supplier/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import SupplierListV1
 
 urlpatterns = [
-    path(f'supplier', SupplierListV1.as_view(), name='supplier_list'),
+    path(f'fornecedores', SupplierListV1.as_view(), name='supplier_list'),
 ]
