f804b80af1ae4d817be1725a5374a349d1276c50
Ananias Raphael <ananias.nobrega@gmail.com>
2024-08-17 16:35:35 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Executa as migrações do banco de dados
 python manage.py migrate
-
-# Cria o superusuário se ele não existir
 python manage.py create_superuser_if_not_exists
-
-# Inicia a aplicação
 gunicorn --bind 0.0.0.0:8000 energy.wsgi:application
