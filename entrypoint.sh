a495e9bbf217a5be85564b247127ddfa7764fcc6
Ananias Raphael <ananias.nobrega@gmail.com>
2024-08-17 16:33:38 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 python manage.py migrate
 
 # Cria o superusuário se ele não existir
-python manage.py create_superuser
+python manage.py create_superuser_if_not_exists
+
 # Inicia a aplicação
-gunicorn --bind 0.0.0.0:8000 energy.wsgi
+gunicorn --bind 0.0.0.0:8000 energy.wsgi:application
